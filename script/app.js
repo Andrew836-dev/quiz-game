686e4a32ed884fc00e52b672c818301b524f9be3
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-08 16:27:20 +0930

    Finished styling, high scores, start button. Made highscores with the most recent name stand out

--- script/app.js
+++ script/app.js
@@ -4,93 +4,44 @@ var headEl = document.getElementById('title');
 var pEl = document.getElementById('info-text');
 var scoreEl = document.getElementById('score-text');
 var timeEl = document.getElementById('time-text');
-var score, currentQuestion, totalSeconds, timer, timeOut, questions, pause;
+var startButton = document.createElement('button');
+var score, currentQuestion, totalSeconds, timer, timeOut, questions, pause, newHigh, highList, state;
 
 init();
 
 document.addEventListener('click', handleClick);
+document.addEventListener('keydown', handleKey);
 
 function init() {
+    state = "start-menu";
+    headEl.textContent = "Javascript Quizzer";
+    questionList.innerHTML = "";
+    startButton.textContent = "Click to Begin!";
+    questionList.append(startButton);
+}
+
+function start() {
+    state = "quiz";
     pause = false;
     setQuestions();
     totalSeconds = 100;
     timer = setInterval(updateTime, 1000);
     score = 0;
     currentQuestion = 0;
-    headEl.textContent = "Questionaire for homework";
+    questionList.classList.add('row');
     renderQuestion(0);
 }
 
-function setQuestions() {
-    questions = [
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "how much wood would a wood chuck chuck?",
-            a: ["if a wood chuck could chuck wood",
-                "a wood chuck would chuck",
-                "as much as a wood chuck could chuck",
-                "if a wood chuck could chuck wood"],
-            correctIndex: 0
-        },
-        {
-            q: "In Javascript, anything between quotation marks \"\" is a ______",
-            a: ["Operation",
-                "String",
-                "Sentence",
-                "Thread"],
-            correctIndex: 1
-        }];
-}
-
 function handleClick(event) {
-    if (event.target.matches('li') && !pause) {
+    if (event.target.matches('button')) {
+        if (state == 'start-menu') {
+            start();
+        }
+        else if (state == 'high-scores') {
+            init();
+        }
+    }
+    else if (event.target.matches('li') && !pause) {
         chosenAnswer = event.target.getAttribute('data-index');
         if (chosenAnswer == questions[currentQuestion].correctIndex) {
             correct(event.target);
@@ -100,32 +51,100 @@ function handleClick(event) {
             questionList.children[questions[currentQuestion].correctIndex].classList.add('correct');
             wrong(event.target);
         }
-        console.log("pausing");
+        // console.log("pausing");
         pause = true;
         timeOut = setTimeout(chooseNextQuestion, 1000);
     }
 }
 
+function handleKey(event) {
+    if (event.target.matches('input')) {
+        if (event.keyCode == 13) {
+            addHighScore(event.target.value.trim());
+            displayHighScore();
+        }
+    }
+}
+
+function addHighScore(string) {
+    newHigh = [string, (score + totalSeconds)];
+    highList = retrievHighScores();
+    if (highList) {
+        highList.push(newHigh);
+        sortScores(highList);
+    }
+    else {
+        highList = [newHigh];
+    }
+    setHighScores(highList);
+}
+
+function displayHighScore() {
+    highList = retrievHighScores();
+    var button = document.createElement('button');
+    state = "high-scores";
+    questionList.innerHTML = "";
+    highList.forEach(element => {
+        var li = document.createElement('li');
+        li.textContent = element[0] + ", " + element[1];
+        if (element[0] == newHigh[0]) {
+            li.classList.add('correct');
+            if (element[1] == newHigh[1]) {
+                li.textContent = "---> " + li.textContent + " <---";
+            }
+        }
+        else {
+            li.classList.add('wrong');
+        }
+        questionList.append(li);
+    });
+    button.textContent = "Back to start";
+    questionList.append(button);
+}
+
+function setHighScores(scoreArray) {
+    localStorage.setItem("highScores", JSON.stringify(scoreArray));
+}
+
+function retrievHighScores() {
+    return JSON.parse(localStorage.getItem("highScores"));
+}
+
+function sortScores(scoreArray) {
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] && scoreArray[i + 1] && scoreArray[i][1] < scoreArray[i + 1][1]) {
+                swapped = true;
+                console.log('swapping');
+                var temp = scoreArray[i];
+                scoreArray[i] = scoreArray[i + 1];
+                scoreArray[i + 1] = temp;
+            }
+        }
+    } while (swapped)
+    if (scoreArray.length > 10) {
+        scoreArray.splice(10);
+    }
+}
+
 function correct(element) {
-    // if (!pause) {
     element.classList.add('correct');
     console.log('Hooray!');
     score++;
-    // }
     renderScore();
 }
 
 function wrong(element) {
-    // if (!pause) {
     element.classList.add('wrong');
     console.log('Doh!');
-    if (totalSeconds > 5) {
-        totalSeconds -= 5;
+    if (totalSeconds > 4) {
+        totalSeconds -= 4;
     }
     else {
         totalSeconds = 0;
     }
-    // }
 }
 
 function renderScore() {
@@ -140,7 +159,7 @@ function chooseNextQuestion() {
     else {
         endScreen();
     }
-    console.log("unpausing");
+    // console.log("unpausing");
     pause = false;
 }
 
@@ -161,20 +180,25 @@ function renderQuestion(index) {
         var li = document.createElement('li');
         li.textContent = questions[index].a[i];
         li.setAttribute('data-index', i);
+        li.classList.add('col-sm-5');
         questionList.appendChild(li);
     }
 }
 
 function endScreen() {
+    var li = document.createElement('li');
+    var textField = document.createElement('input');
+    var pEl2 = document.createElement('p');
     pause = true;
     clearInterval(timer);
     questionList.innerHTML = "";
+    questionList.classList.remove('row');
     questionBox.textContent = "Thanks for playing";
-    var li = document.createElement('li');
     li.style.listStyle = "none";
-    li.textContent = `Your final Score was ${score} out of ${questions.length}`;
+    li.textContent = `Your final Score was ${score} out of ${questions.length}\nPlus ${totalSeconds} for your remaining time.`;
     questionList.append(li);
-    var button = document.createElement('button');
-    button.textContent = "Play again?";
-    questionList.append(button);
+    textField.setAttribute("type", "text")
+    questionList.appendChild(textField);
+    pEl2.textContent = "Enter your initials to view high scores.";
+    questionList.appendChild(pEl2);
 }
---
+++ script/setQuestions.js
@@ -0,0 +1,66 @@
+function setQuestions() {
+    questions = [
+        {
+            q: "Arrays in Javascript can be used to contain _____.",
+            a: ["Numbers",
+                "Strings",
+                "Objects",
+                "All of the above"],
+            correctIndex: 3
+        },
+        {
+            q: "What is the best construction of a for loop?",
+            a: ["( var i = 0; i < targetnumber; i++ )",
+                "( i = target; i === true )",
+                "( var i = 0; i < 5 ; i-- )",
+                "( ; false;  )"],
+            correctIndex: 0
+        },
+        {
+            q: "What is the property to retrieve or change the text inside of a text input field?",
+            a: [".textContent",
+                ".value",
+                ".content",
+                ".text"],
+            correctIndex: 1
+        },
+        {
+            q: "Which of these variables accesses the root of the DOM?",
+            a: ["body",
+                "html",
+                "document",
+                "head"],
+            correctIndex: 2
+        },
+        {
+            q: "Console.log() is a useful tool for debugging.",
+            a: ["True",
+                "False"],
+            correctIndex: 0
+        },
+        {
+            q: "An Event _____ can be used to react to mouse clicks and key presses",
+            a: ["Listener",
+                "Senser",
+                "Detecter",
+                "Fastener"],
+            correctIndex: 0
+        },
+        {
+            q: "Javascript is a _____ Oriented Programming language",
+            a: ["Data",
+                "Function",
+                "Object",
+                "People"],
+            correctIndex: 2
+        },
+        {
+            q: "In Javascript, anything between quotation marks \"\" is a ______",
+            a: ["Operation",
+                "String",
+                "Sentence",
+                "Thread"],
+            correctIndex: 1
+        }
+    ];
+}
